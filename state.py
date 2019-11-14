cd986acf0519a538492480ca8193424e7a779125
dannabel <dominick.annabel@ibm.com>
2019-11-13 22:36:05 -0500

    changed Directions to DIRECTIONS

--- state.py
+++ state.py
@@ -8,7 +8,7 @@ RIGHT = 1
 UP = 2
 DOWN = 3
 
-Directions = [LEFT, RIGHT, UP, DOWN]
+DIRECTIONS = [LEFT, RIGHT, UP, DOWN]
 
 class State:
     def __init__(self):
@@ -17,7 +17,6 @@ class State:
         self.food_angle = 0.0  # type: float
 
     def get_blocked_dirs(self):
-        for
         point = np.array(snake[0]) + np.array(direction)
         return point.tolist() in snake[:-1] or point[0] == 0 or point[1] == 0 or point[0] == 21 or point[1] == 21
 
